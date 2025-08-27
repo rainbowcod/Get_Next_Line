6410b077c92def1c1fe664eb20a2a1aecc9c3431
rainbowcod <rainbowmelone01@gmail.com>
2025-08-27 15:13:08 +0200

    inrfinite loop in leftovers

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 00:50:37 by olmatske          #+#    #+#             */
-/*   Updated: 2025/08/27 11:47:56 by olmatske         ###   ########.fr       */
+/*   Updated: 2025/08/27 12:36:05 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include "get_next_line.h"
 
-static char	*copy_buffer(char *buffer, int amount, char *stash, char *leftovers);
+static char	*copy_buffer(char *buffer, int amount, char *stash, char **leftovers);
 
 char	*get_next_line(int fd)
 {
@@ -25,38 +25,55 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	if (leftovers)
+		stash = ft_strdup(leftovers);
+	else
+	{
+		stash = malloc(1);
+		if (!stash)
+			return (NULL);
+	}
 	while (ft_strchr(buffer, '\n') == -1)
 	{
 		amount = read(fd, buffer, BUFFER_SIZE);
 		if (amount == -1)
 			return (NULL);
-		buffer[amount + 1] = '\0';
-		stash = malloc(sizeof(char) * amount);
-		stash = copy_buffer(buffer, amount, stash, leftovers);
+		if (amount == 0)
+			return (leftovers);
+		buffer[amount] = '\0';
+		stash = copy_buffer(buffer, amount, stash, &leftovers);
+	}
+	if (stash[0] == '\0')
+	{
+		//free(stash);
+		return (NULL);
 	}
 	return (stash);
 }
 
-static char	*copy_buffer(char *buffer, int amount, char *stash, char *leftovers)
+static char	*copy_buffer(char *buffer, int amount, char *stash, char **leftovers)
 {
-	int	start;
+	int		start;
+	char	*tmp;
 
 	start = 0;
 	if (ft_strchr(buffer, '\n') > -1)
 	{
 		start = ft_strchr(buffer, '\n');
-		if (leftovers)
+		if (*leftovers)
 		{
-			free(stash);
-			stash = ft_strjoin(leftovers, ft_substr(buffer, 0, start));
-			free(leftovers);
+			//free(stash);
+			stash = ft_strjoin(*leftovers, ft_substr(buffer, 0, start));
+			//free(leftovers);
 		}
 		stash = ft_strjoin(stash, ft_substr(buffer, 0, start));
-		leftovers = ft_substr(buffer, start + 1, amount);
+		*leftovers = ft_substr(buffer, start + 1, amount);
 	}
 	else
 	{
-		stash = ft_strjoin(stash, buffer); //memory leak
+		tmp = ft_strjoin(stash, buffer);
+		//free(stash);
+		stash = tmp;
 	}
 	return (stash);
 }
@@ -102,14 +119,16 @@ int	main (void)
 {
 	int	fd;
 	char *line;
+	int i = 0;
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
 		return (printf("error"));
-	while ((line = get_next_line(fd)) != NULL)
+	while ((line = get_next_line(fd)) != NULL || i < 30)
 	{
 		printf("%s", line);
-		free(line);
+		i++;
+		//free(line);
 	}
 	close(fd);
 	return (0);
