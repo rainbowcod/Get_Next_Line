223ba0e7be17ce650fed0ed5b716751c51a76dbe
Olga Matskevich <olmatske@2-B-1.42heilbronn.de>
2025-09-02 13:54:48 +0200

    testing code with paco

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,99 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 21:34:01 by olmatske          #+#    #+#             */
-/*   Updated: 2025/09/02 12:59:12 by olmatske         ###   ########.fr       */
+/*   Updated: 2025/09/02 13:53:59 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// problems: where do I read and put in tmp and clean buffer???
 
-char	*get_line(int fd, char *tmp, char *buffer, int amount);
 
-char	*get_next_line(int fd)
-{
-	static char	buffer[BUFFER_SIZE + 1];
-	char		*tmp;
-	char		*res;
-	int			amount;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	amount = 1;
-	while (amount > 0)
-	{
-		amount = read(fd, buffer, BUFFER_SIZE);
-		tmp = ft_strdup(buffer);
-		if (!tmp)
-			return (NULL);
-		ft_bezero(buffer, amount); // use BUFFER_SIZE but why?
-		res = get_line(fd, tmp, buffer, amount);
-		if (!res)
-			return (NULL);
-	}
-}
-char	*get_line(int fd, char *tmp, char *buffer, int amount)
-{
-	char	*res;
-	int		nl;
 
-	while (amount > 0)
-	{
-		if (ft_strchr(tmp, '\n') > -1)
-		{
-			nl = ft_strchr(tmp, '\n');
-			res = ft_substr(tmp, 0, nl + 1);
-			ft_strlcpy(buffer, tmp + nl + 1, BUFFER_SIZE);
-		}
-	}
-}
+
+
+
+
+// problems: check for values in buffer
+
+// char	*get_line(int fd, char *tmp, char *buffer, int amount);
+// char	*read_file(int fd, char *tmp, char *buffer, int amount);
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	buffer[BUFFER_SIZE + 1];
+// 	char		*tmp;
+// 	char		*res;
+// 	int			amount;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+	
+// 	return (res);
+// }
+// char	*read_file(int fd, char *tmp, char *buffer, int amount)
+// {
+// 	amount = 1;
+
+// 	if new line in buffer
+// 		return get_line
+// 	while (amount > 0)
+// 	{
+// 		if (res)
+// 		{
+// 			free (res);
+// 			res = NULL;
+// 		}
+// 		if (buffer not empty)
+// 			tmp = ft_strdup(buffer);
+		
+// 		amount = read(fd, buffer, BUFFER_SIZE);
+// 		buffer[amount] = '\0';
+// 		if (!tmp)
+// 			return (NULL);
+// 		// ft_bezero(buffer, amount); // use BUFFER_SIZE but why?
+// 		res = get_line(fd, tmp, buffer, amount); // check if result is empty, if not -> free
+// 		if (!res)
+// 			return (NULL);
+// 	}
+// }
+
+
+// char *read_file:
+// 	if (new line in buffer)
+// 		return strdup(buffer)
+// 	else
+// 		while()
+// read from file
+// extratc from readed
+// update buffer
+
+// char	*get_line(int fd, char *tmp, char *buffer, int amount)
+// {
+// 	char	*res;
+// 	int		nl;
+
+// 	while (amount > 0 || ft_strchr(tmp, '\n') > -1)
+// 	{
+// 		if (ft_strchr(tmp, '\n') > -1)
+// 		{
+// 			nl = ft_strchr(tmp, '\n');
+// 			res = ft_substr(tmp, 0, nl + 1);
+// 			ft_strlcpy(buffer, tmp + nl + 1, BUFFER_SIZE);
+// 			// set whole buffer array to \0
+// 			free (tmp);
+// 			tmp = NULL;
+// 			return (res);
+// 		}
+// 		else
+// 		{
+// 			res = ft_strjoin(res, tmp);
+// 			ft_bezero(buffer, amount);
+// 		}
+// 	}
+// 	return (res);
+// }
 
 void	*ft_bezero(char *str, size_t len)
 {
--- tst
+++ tst
@@ -1,9 +1,55 @@
 
+char    *get_line(char *stash);
 
+char    *get_next_line(int fd)
+{
+    static char    stash[BUFFER_SIZE + 1];
+    char           buff[BUFFER_SIZE + 1];
+    char           *tmp;
+    char           *line;
+    ssize_t        bytes;
 
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    bytes = 1;
+    while (!ft_strchr(stash, '\n') && bytes > 0)
+    {
+        bytes = read(fd, buff, BUFFER_SIZE);
+        if (bytes < 0)
+            return (NULL);
+        buff[bytes] = '\0';
+        if (!(tmp = ft_strjoin(stash, buff)))
+            return (NULL);
+        ft_strlcpy(stash, tmp, BUFFER_SIZE + 1);
+        free(tmp);
+    }
+    if (stash[0] == '\0')
+        return (NULL);
+    return (get_line(stash));
+}
+char    *get_line(char *stash)
+{
+    char    *line;
+    char    *newstash;
+    int     nl;
 
-
-
-
-
-
+    nl = ft_strchr(stash, '\n');
+    if (nl < 0)
+    {
+        line = ft_strdup(stash);
+        stash[0] = '\0';
+        return (line);
+    }
+    if (!(line = ft_substr(stash, 0, nl + 1)))
+        return (NULL);
+    if (!(newstash = ft_substr(stash,
+                nl + 1,
+                ft_strlen(stash) - nl - 1)))
+    {
+        free(line);
+        return (NULL);
+    }
+    ft_strlcpy(stash, newstash, BUFFER_SIZE + 1);
+    free(newstash);
+    return (line);
+}
